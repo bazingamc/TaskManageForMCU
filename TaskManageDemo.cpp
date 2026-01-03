372e477a5ec83d806a80a1a1ae2809bdf0b1b188
miaoce <miaoce@qq.com>
2026-01-03 15:12:30 +0800

    Demo增加注释

--- TaskManageDemo.cpp
+++ TaskManageDemo.cpp
@@ -1,20 +1,18 @@
-﻿// TaskManage.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// TaskManageDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
 #include <windows.h>
 #include <mmsystem.h>
-#include "task.h"
+#include ".\task\task.h"
+
+
+// windows定时器
 
 #pragma comment(lib, "winmm.lib") // 链接多媒体库
-
-
-
-// 回调函数
 void CALLBACK TimerProc(UINT uTimerID, UINT uMsg, DWORD_PTR dwUser, DWORD_PTR dw1, DWORD_PTR dw2) {
 	task::now_time +=1000;
 }
-
 void timer_init()
 {
 	// 设置定时器分辨率为 1ms
@@ -35,27 +33,28 @@ void timer_init()
 	}
 }
 
-
 void task1(task* self, TaskParam* param);
 void task2(task* self, TaskParam* param);
-task t1("t1", task1);
-task t2("t2", task2);
+
+task t1("t1", task1);//创建任务1
+task t2("t2", task2);//创建任务2
+
 
 void task1(task* self, TaskParam* param)
 {
 	switch (self->GetUserState())
 	{
 	case 0:
-		self->TransitionToNextState();
+		self->TransitionToNextState();//跳转到下一个状态
 		break;
 	case 1:
-		self->Delay(2000, WHERE_NEXT);
+		self->Delay(2000, WHERE_NEXT);//延迟2秒后跳转到下一个状态
 		break;
 	case 2:
-		self->SubtaskStart(&t2, param, 0, WHERE_NEXT, WHERE_FAIL);
+		self->SubtaskStart(&t2, param, 0, WHERE_NEXT, WHERE_FAIL);//启动子任务t2，成功后跳转到下一个状态，失败后跳转到失败状态
 		break;
 	case 3:
-		self->UserStateChange(0);
+		self->UserStateChange(0);//回到初始状态，重新开始
 
 	default:
 		break;
@@ -81,19 +80,21 @@ void task2(task* self, TaskParam* param)
 	}
 }
 
-
 int main()
 {
-	timer_init();// 初始化定时器
+	// 初始化定时器
+	timer_init();
 
+	//启动任务
 	TaskParam p1;
 	p1.father = nullptr;
 	p1.data = nullptr;
 	p1.data_len = 0;
-	t1.Start(&p1, 0);//启动任务
+	t1.Start(&p1, 0);
 
 	while (1)
 	{
 		task::TaskRun();
 	}
 }
+
